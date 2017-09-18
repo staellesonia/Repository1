cb55a404c394e33fb3ca0b9af248ff13f49a68b6
Staelle MAKAMTE <staellesonia@yahoo.fr>
2017-09-18 15:42:28 +0200

    deuxieme commit

--- voiture.php
+++ voiture.php
@@ -4,15 +4,20 @@ class Voiture
 {
   public $type;
   public $color;
+  public $prix;
 
-  function __construct($init_type, $init_color)
+  function __construct($init_type, $init_color, $init_prix)
   {
-    $this->type = $init_type;
+    $this->type  = $init_type;
     $this->color = $init_color;
+    $this->prix  = $init_prix;
   }
   function getBrand ($brand)
-  { }
+  {
+    $this->getBrand = $brand
+  }
 }
-$voiture = new Voiture('VW', 'Rouge');
+$voiture = new Voiture('VW', 'Rouge', '50euros');
 var_dump($voiture);
 // echo $voiture->getBrand($brand);
+?>
